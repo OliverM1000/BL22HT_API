7495e62dd711440c2548550987d75edc6c611d89
Oliver Mueller <oliver.mueller.1000@hotmail.com>
2024-06-28 21:17:39 -0700

    removed password in json

--- routes/sampleFrames.js
+++ routes/sampleFrames.js
@@ -33,7 +33,7 @@ router.get("/:tag", auth, async (req, res) => {
 router.get("/:tag", async (req, res) => {
     const sampleFrame = await SampleFrame
     .findOne({ tag: req.params.tag })
-    .populate({path: 'user'})
+    .populate({path: 'user'}).select('-password')
     .populate({ path: 'samplePlateL', populate: { path: 'samples', populate: { path: 'scanSetups' } } })
     .populate({ path: 'samplePlateR', populate: { path: 'samples', populate: { path: 'scanSetups' } } });
     
